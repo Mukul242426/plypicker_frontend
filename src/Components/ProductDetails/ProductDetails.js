5860e55cc6ce9c1c0225b87b2c86111e95584532
Mukul <mukulsmart26@gmail.com>
2024-03-17 16:27:03 +0530

    view a product functionality done

--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -1,10 +1,37 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import styles from "./ProductDetails.module.css";
 import Header from "../Header/Header";
+import {FRONTEND_URL} from '../../utils/utils'
+import axios from 'axios'
+import { useParams } from "react-router-dom";
 
 function ProductDetails() {
 
+    const {id}=useParams()
+
    const [data,setData]=useState({});
+
+   useEffect(()=>{
+    const fetchData=async()=>{
+  
+        const jwttoken = JSON.parse(localStorage.getItem("token"));
+
+        try{
+            const response=await axios.get(`${FRONTEND_URL}/product/${id}`,{
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${jwttoken}`,
+                }})
+            console.log(response)
+            setData(response.data.product)
+
+        }catch(error){
+            console.log(error)
+
+        }
+    }
+    fetchData()
+   },[])
 
   return (
     <div className={styles.product}>
@@ -14,13 +41,14 @@ function ProductDetails() {
       <div className={styles.container_2}>
         <div className={styles.product_details_container}>
           <div className={styles.product_image}>
-            <img src={image} alt={productName} />
+            <img src={data.image} alt={data.productName} />
           </div>
           <div className={styles.product_info}>
-            <h1 className={styles.product_name}>{productName}</h1>
-            <p className={styles.price}>${price}</p>
-            <p className={styles.product_description}>{productDescription}</p>
-            <p className={styles.department}>Department: {department}</p>
+            <h1 className={styles.product_name}>{data.productName}</h1>
+            <p className={styles.price}>${data.price}</p>
+            <p className={styles.product_description}>{data.productDescription}</p>
+            <p className={styles.department}>Department: {data.department}</p>
+            <button className={styles.edit_button}>Edit Product</button>
           </div>
         </div>
       </div>
